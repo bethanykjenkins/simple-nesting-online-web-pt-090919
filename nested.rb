08cc8bc7fd4a9643447626e2d815101a8b8f318c
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-02 02:28:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash [:dennis_ritchie][:languages]
+     array =programmer_hash [:dennis_ritchie][:languages]
+     array.join
 end
 
 def adding_matz
@@ -83,7 +84,7 @@ def adding_matz
      }
 
     programmer_hash[:yukihiro_matsumoto]={:known_for => "Ruby", :languages => ["LISP", "C"]}
-    
+    programmer_hash
 end
 
 def changing_alan
@@ -105,7 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info     
-     
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -125,4 +126,5 @@ def adding_to_dennis
         }
      }
 programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash
 end
